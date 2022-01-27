93ad4db1b83dd60bdbac68fec57468964dcfcecb
Richard Quadling <RQuadling@GMail.com>
2022-01-27 12:35:13 +0000

    Final cleanup for Terraform upgrade

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "external" "awscli_program" {
     role_session_name  = var.role_session_name
     aws_cli_commands   = join(" ", var.aws_cli_commands)
     aws_cli_query      = var.aws_cli_query
-    output_file        = format("%s/temp/%s.json", path.module, var.role_session_name)
+    output_file        = format("%s/temp/results.json", path.module)
     debug_log_filename = var.debug_log_filename
   }
 }
@@ -13,4 +13,10 @@ data "external" "awscli_program" {
 data "local_file" "awscli_results_file" {
   depends_on = [data.external.awscli_program]
   filename   = data.external.awscli_program.query.output_file
+}
+
+output "result" {
+  depends_on  = [data.local_file.awscli_results_file]
+  description = "The output of the AWS CLI command"
+  value       = jsondecode(data.local_file.awscli_results_file.content)
 }
--- output.tf
+++
@@ -1,4 +0,0 @@
-output "result" {
-  description = "The output of the AWS CLI command"
-  value       = jsondecode(data.local_file.awscli_results_file.content)
-}
